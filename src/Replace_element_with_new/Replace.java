a38e470abd55a1309426efd15578c909ca3ee3f4
dipanshukale <dipanshu2003@gmail.com>
2023-09-12 18:36:19 +0530

    java practice

---
+++ src/Replace_element_with_new/Replace.java
@@ -0,0 +1,51 @@
+package Replace_element_with_new;
+
+import java.util.Scanner;
+
+public class Replace {
+    public static void main(String[] args) {
+        int num, element,user, value;
+         int arr[] = new int[10];
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter Your Array Numbers : ");
+
+
+        for (int i = 0; i< 10; i++){
+            arr[i] = scan.nextInt();
+
+
+        }
+
+
+
+
+        System.out.println("Enter same Number :  ");
+        user = scan.nextInt();
+        System.out.println("Enter new-element : ");
+        element = scan.nextInt();
+
+        for (int i =0; i<10; i++) {
+
+            if (arr[i] == user){
+
+
+
+                arr[i]= element;
+                System.out.println(arr[i]);
+
+            } else if (arr[i] != user) {
+
+                System.out.println(arr[i]);
+
+
+
+            }
+
+
+        }
+       
+
+    }
+}
+
