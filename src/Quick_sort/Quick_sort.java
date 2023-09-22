5d6d26af4e801ebfce9dae55000dfad6af745118
dipanshukale <dipanshu2003@gmail.com>
2023-09-22 16:47:36 +0530

    java practice

---
+++ src/Quick_sort/Quick_sort.java
@@ -0,0 +1,16 @@
+package Quick_sort;
+
+import java.util.Scanner;
+
+public class Quick_sort {
+    public static void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Array length : ");
+        n = scan.nextInt();
+
+        int []arr = new int[n];
+
+
+    }
+}
